0b3abde2eeaef9094040be31fa1c3497625f5e3c
Amit Apple <amitap@microsoft.com>
2013-02-05 09:41:03 -0800

    MvcWithXUnitTests

---
+++ XUnitTests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace XUnitTests
+{
+    public class Tests
+    {
+        [Fact]
+        public void ThisTestShouldFail()
+        {
+            Assert.Equal(4, 2 + 1);
+        }
+    }
+}
